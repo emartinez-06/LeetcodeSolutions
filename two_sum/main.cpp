28cd365b2b4bcd4a1e2259040aaca04c4c513aea
Erick Martinez <erickmartinez0131@gmail.com>
2025-07-19 21:09:00 -0500

    comments

--- two_sum/main.cpp
+++ two_sum/main.cpp
@@ -8,8 +8,11 @@ vector<int> twoSum(vector<int> &nums, int target) {
     unordered_map<int, int> num_map;
 
     for (int i = 0; i < nums.size(); ++i) {
+        // iterates through nums vector and calculates complement
         int complement = target - nums[i];
 
+        // if complement already in map done
+        // if not insert curr num and ndx into map
         if (num_map.find(complement) != num_map.end()) {
             return {num_map[complement], i};
         }
