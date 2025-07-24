a2cca8c5f66e30f4ad41363d2857630de73e67ac
Erick Martinez <erickmartinez0131@gmail.com>
2025-07-24 13:24:30 -0500

    Add Two Numbers

--- add_two_numbers/main.cpp
+++ add_two_numbers/main.cpp
@@ -2,6 +2,21 @@
 
 using namespace std;
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+   public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {}
+};
+
 int main() {
     cout << "Hello World!\n";
 
