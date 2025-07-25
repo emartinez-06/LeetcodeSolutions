c56b9e6d22c19358207b078e1d3e256988ab2be0
Erick Martinez <erickmartinez0131@gmail.com>
2025-07-25 08:52:26 -0500

    completed

--- add_two_numbers/main.cpp
+++ add_two_numbers/main.cpp
@@ -2,19 +2,43 @@
 
 using namespace std;
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
    public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {}
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *tempHead = new ListNode();
+        ListNode *currNode = tempHead;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int val1 = 0;
+            int val2 = 0;
+
+            if (l1 != nullptr) {
+                val1 = l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != nullptr) {
+                val2 = l2->val;
+                l2 = l2->next;
+            }
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            int currVal = sum % 10;
+
+            currNode->next = new ListNode(currVal);
+            currNode = currNode->next;
+        }
+        return tempHead->next;
+    }
 };
 
 int main() {
