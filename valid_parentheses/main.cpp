40b074351d3cbde180cec24c6f923174110d937f
Erick Martinez <erickmartinez0131@gmail.com>
2025-07-31 09:37:11 -0500

    Intial commit

---
+++ valid_parentheses/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+   public:
+    bool isValid(string s) {
+        int bracket = 0;
+        int parentheses = 0;
+        int curl = 0;
+        for (int i = 0; i <= s.size() - 1; ++i) {
+            if (s.at(i) == '[' || s.at(i) == ']') {
+                bracket += 1;
+            } else if (s.at(i) == '{' || s.at(i) == '}') {
+                curl += 1;
+            } else if (s.at(i) == '(' || s.at(i) == ')') {
+                parentheses += 1;
+            }
+        }
+        if (curl % 2 == 0 && bracket % 2 == 0 && parentheses % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+};
+int main() {
+    cout << "valid_parenteses\n";
+
+    return 0;
+}
