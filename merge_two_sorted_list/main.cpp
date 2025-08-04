39d9e57e4871beaba3f6754ff9f8fb7ab338d10c
Erick Martinez <erickmartinez0131@gmail.com>
2025-08-04 09:43:49 -0500

    completed

--- merge_two_sorted_list/main.cpp
+++ merge_two_sorted_list/main.cpp
@@ -2,8 +2,43 @@
 
 using namespace std;
 
-int main() {
-    cout << "merge_two_sorted_lists\n";
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-    return 0;
-}
+class Solution {
+   public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+        ListNode *currNode;
+
+        if (!list1)
+            return list2;
+        if (!list2)
+            return list1;
+
+        ListNode dummy;
+        ListNode *tail = &dummy;
+
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                tail->next = list1;
+                list1 = list1->next;
+            } else {
+                tail->next = list2;
+                list2 = list2->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = list1 ? list1 : list2;
+        return dummy.next;
+    };
+
+    int main() {
+        cout << "merge_two_sorted_lists\n";
+
+        return 0;
+    }
