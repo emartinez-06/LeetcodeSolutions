f9b248d82089fdadc539ddce428ac8d07ea17cf2
Erick Martinez <erickmartinez0131@gmail.com>
2025-08-05 18:00:59 -0500

    completed

--- remove_duplicates_from_sorted_array/main.cpp
+++ remove_duplicates_from_sorted_array/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class Solution {
+   public:
+    int removeDuplicates(vector<int> &nums) {
+        int slow = 0;
+
+        for (int fast = 1; fast < nums.size(); ++fast) {
+            if (nums[fast] != nums[slow]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+        return slow + 1;
+    }
+};
+
 int main() {
-    cout << "test\n";
+    Solution sol;
+    vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4};
+
+    int k = sol.removeDuplicates(nums);
+
+    cout << "Number of unique elements (k); " << k << endl;
 
     return 0;
 }
