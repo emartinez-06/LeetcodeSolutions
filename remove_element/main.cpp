6eba66a441f9ab91477a1d0395fb06eb5e108ff2
Erick Martinez <erickmartinez0131@gmail.com>
2025-08-06 09:03:51 -0500

    completed

--- remove_element/main.cpp
+++ remove_element/main.cpp
@@ -3,7 +3,26 @@
 
 using namespace std;
 
+class Solution {
+   public:
+    int removeElement(vector<int> &nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
+
 int main() {
-    cout << "remove_element\n";
+    vector<int> nums = {0, 1, 2, 2, 3, 0, 4, 2};
+    int val = 2;
+
+    Solution sol;
+    int k = sol.removeElement(nums, val);
+    cout << "k = " << k << endl;
     return 0;
 }
