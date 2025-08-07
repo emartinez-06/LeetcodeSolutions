585236824afbaa9bb116b1274a629b53df871681
Erick Martinez <erickmartinez0131@gmail.com>
2025-08-07 09:10:56 -0500

    Intial Commit

---
+++ search_insert_position/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+   public:
+    int searchInsert(vector<int> &nums, int target) {}
+};
+
+using namespace std;
+
+int main() {
+    cout << "search_insert_position\n";
+
+    return 0;
+}
