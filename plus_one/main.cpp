8421547c8cd59d1e3aff8f21f684b60673725a8a
Erick Martinez <erickmartinez0131@gmail.com>
2025-08-10 10:32:03 -0500

    Intial commit

---
+++ plus_one/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    vector<int> plusOne(vector<int> &digits) {}
+};
+
+int main() {
+    cout << "plus_one\n";
+
+    return 0;
+}
