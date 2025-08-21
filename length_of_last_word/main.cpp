bf35175d849cd7d5d2760b914fc6e507508f0b1f
Erick Martinez <erickmartinez0131@gmail.com>
2025-08-21 13:35:33 -0500

    completed

--- length_of_last_word/main.cpp
+++ length_of_last_word/main.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 class Solution {
    public:
-    int lengthOfLastWord(string s) {}
+    int lengthOfLastWord(string s) {
+        int n = s.size();
+        int x = 0;
+
+        for (int i = n - 1; i >= 0; --i) {
+            if (s[i] == ' ') {
+                if (x > 0)
+                    break;
+            } else {
+                ++x;
+            }
+        }
+        return x;
+    }
 };
 
 int main() {
