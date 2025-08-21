f68dc79c6ef73e2e27ea11bfd687a3abc6a9df35
Erick Martinez <erickmartinez0131@gmail.com>
2025-08-21 13:29:59 -0500

    Intia; commit

---
+++ length_of_last_word/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+   public:
+    int lengthOfLastWord(string s) {}
+};
+
+int main() {
+    cout << "length_of_last_word\n";
+
+    return 0;
+}
